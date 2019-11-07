f3ce331d2a5d8c64ee81c54c0723728d25a15c42
BEN <wahudong@email.com>
2019-11-07 13:58:50 -0600

    added static page table to ActiveAdmin

---
+++ app/admin/pages.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Page do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :content, :about_us, :permalink
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:content, :about_us, :permalink]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'products/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root to: 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
