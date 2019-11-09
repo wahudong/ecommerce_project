c65a46450006b762b4d36dc45d7de8642d37a06c
BEN <wahudong@email.com>
2019-11-09 01:08:20 -0600

    add navbar and creat Page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'page/index'
+  get 'page/show'
   get 'products/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
---
+++ test/controllers/page_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PageControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get page_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get page_show_url
+    assert_response :success
+  end
+
+end
