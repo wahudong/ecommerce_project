67d8ffc8544076671ecbe00e407b2bf20c534a9a
BEN <wahudong@email.com>
2019-11-27 12:45:15 -0600

    just added order module into Active Admin for monitoring

---
+++ app/admin/orders.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Order do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :customer_id, :street_no, :street_name, :city, :province, :postal_cade, :status, :pst, :gst, :total_amount
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:customer_id, :street_no, :street_name, :city, :province, :postal_cade, :status, :pst, :gst, :total_amount]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
